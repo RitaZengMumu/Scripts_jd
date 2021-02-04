5f46897fa5ee49f1461384c078540673900c1697
RitaZengMumu <rita_zengmumu@hotmail.com>
2021-02-04 09:50:28 +0800

    Update USER_AGENTS.js

--- USER_AGENTS.js
+++ USER_AGENTS.js
@@ -1,5 +1,6 @@
 const USER_AGENTS = [
-  "jdapp;android;9.3.4;10;5343464323935303-3633432353468316;network/wifi;model/YAL-AL00;addressid/1996320260;aid/54d42950c3425d8a;oaid/fde5ff7f-f7bf-3f0c-63da-dfe7f77ebffe;osVer/29;appBuild/86388;partner/huawei;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 10; YAL-AL00 Build/HUAWEIYAL-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36"
+  "jdapp;android;9.3.4;10;5343464323935303-3633432353468316;network/wifi;model/YAL-AL00;addressid/1996320260;aid/54d42950c3425d8a;oaid/fde5ff7f-f7bf-3f0c-63da-dfe7f77ebffe;osVer/29;appBuild/86388;partner/huawei;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 10; YAL-AL00 Build/HUAWEIYAL-AL00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045230 Mobile Safari/537.36",
+  "Dalvik/2.1.0 (Linux; U; Android 9; Mi Note 3 MIUI/V11.0.4.0.PCHCNXM)"
 ]
 /**
  * 生成随机数字
